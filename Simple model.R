ac16f3a7c73b74f1991d1a8a924544be26c722ca
kingjad33 <142939283+kingjad33@users.noreply.github.com>
2023-09-20 11:00:01 -0500

    Add files via upload

--- Simple model.R
+++ Simple model.R
@@ -1,18 +1,22 @@
 ####Simple model#####
+setwd("C:/Users/jzk0179/OneDrive - Auburn University/Desktop/Classes/WILD7400")
+directory = getwd()
+outdir    = paste(directory,"/output/",sep="")                    #directory to save model output  
+source(paste(directory, "/source/FunctionSourcer.R", sep = ''))   #source functions and set source directory
 
 #Shark age vs time searching
 #age range from 1 to 20
 iage= rnorm(100, mean=10, sd=2)
 hist(iage)
 
-#time searching = 1 to 20 with more variablility
+#time searching relates to age
 time= iage + rnorm(100,0,1)
 hist(iage)
 
 plot(x=iage, y=time, pch=19, col="SeaGreen2", xlim=c(1,20), ylim=c(1,20), cex=.5)
 abline(a=0,b=1)
 
-#Larger size means higher age and more food found. longer search = more food found
+#older shark means more time searching and more food found.
 quant = NULL
 for(i in 1:length(time)){
   quant = c(quant, rnorm(1, mean(time[i]/20), sd=1/20))
@@ -21,32 +25,53 @@ for(i in 1:length(time)){
 plot(x=time, y=quant, pch=19, col="Sienna3", xlim=c(1,20), ylim=c(0,1), cex=.5)
 abline(a=0,b=(.1/2))
 
-K = 200 #carrying capacity
+
+K = 400 #carrying capacity
 maxage = 20 #max age
-maxsearch = 20 #longest search
-searchnum = 1 #1 searches per shark
+maxsearch = 1440 #longest search time in minutes
+searchnum = 1 #Amount of searches in a day
 
+#output
+nreps = 100
+#towrite = c(r,reps,K,maxage,maxsearch,meanweight,meansearch,meanfood)
+
+
+#create data frame
 shark = data.frame(id = seq(1,K,1), age = rep(seq(1,maxage,1),(K/maxage)))
+shark$weight = shark$age*rnorm(K,3,1)
 
-#random search for each 200 induviduals
-search= replicate(200, rnorm(1,10,4))
-search
+#older age spends more time searching
+for(i in 1:nrow(shark)){
+  #function of age to time searching
+  searchtime = shark$age * (shark$weight)
+  
+  shark$search = searchtime
+  
+  shark$search[shark$search < 0]  = 1
+  shark$search[shark$search > 1440]  = 1440
+  
+  shark$food = rep(NA, nrow(shark))
+  shark$food = shark$search/rnorm(K,10,1)
+}
 
-#adding column into dataframe
-shark$search = rep(NA, nrow(shark))
-shark$search = search
+#output
+meanweight <- shark %>% group_by(age) %>% 
+  summarise(mean_weight=mean(weight),
+            .groups = 'drop')
+meansearch <- shark %>% group_by(age) %>% 
+  summarise(mean_search=mean(search),
+            .groups = 'drop')
+meanfood <- shark %>% group_by(age) %>%
+  summarise(mean_fod=mean(food),
+            .groups = 'drop')
 
-#getting rid of negative or too large values
-shark$search[shark$search < 0]  = 0
-shark$search[shark$search > 20] = 20
+towrite = c(r,reps,K,maxage,maxsearch,meanweight,meansearch,meanfood)
 
-#search is based on time per day, generally- larger shark has more time to search
-shark$searchtime = rep(NA, nrow(shark))
-shark$searchtime = shark$search * shark$age
+#create a folder with date and time so it is unique to new folder names
+folder = gsub(" ", "", paste("../Output/output_", Sys.time(), ""), fixed = TRUE)
+dir.create(folder)
 
-#how they catch food in time, every 5 minutes they catch 1 item
-shark$food = rep(NA, nrow(shark))
-shark$food = shark$searchtime/5
+#copy output into folder to use later
+data = read.table("../Output/outputsummary.csv", header=T, sep=",")
+write.table(data, paste("../Output/", folder, "/outputsummary.csv", sep=""), row.names=F, col.names=T, sep=",")  
 
-View(shark)
-plot(food ~ age, data = shark)
